905514f67cfbfb6f8681925357a5285c95f498b0
Sij-Coco <parkourweapon@gmail.com>
2026-03-10 21:50:49 -0800

    item adapter almost finished

--- app/src/main/java/com/example/barangaycomplaints/ItemAdapter.java
+++ app/src/main/java/com/example/barangaycomplaints/ItemAdapter.java
@@ -1,33 +1,56 @@
 package com.example.barangaycomplaints;
 
+import android.view.LayoutInflater;
+import android.view.View.OnClickListener;
+import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.barangaycomplaints.databinding.LayoutItemBinding;
+
 import java.util.List;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
-    private List<Complaint> itemList;
+    private List<Complaint> complaintList;
 
+    private OnClickListener onClickListener;
+
+    public ItemAdapter(List<Complaint> complaintList) {
+        this.complaintList = complaintList;
+    }
+
+    public void setOnClickListener(OnClickListener onClickListener) {
+        this.onClickListener = onClickListener;
+    }
 
 
     @NonNull
     @Override
-    public ItemAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ItemViewHolder(LayoutItemBinding.inflate(LayoutInflater.from(parent.getContext())));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemAdapter.ItemViewHolder holder, int position) {
+        Complaint complaint = complaintList.get(position);
+        holder.binding.tvSubject.setText(complaint.getSubject());
+        holder.binding.tvUsername.setText(complaint.getUser().getUsername());
 
     }
 
     @Override
-    public int getItemCount() {
-        return 0;
+    public int  getItemCount() {
+        return complaintList.size();
     }
 
-    public class ItemViewHolder {
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private final LayoutItemBinding binding;
+        public ItemViewHolder(LayoutItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
     }
 }
--- app/src/main/java/com/example/barangaycomplaints/MainActivity.java
+++ app/src/main/java/com/example/barangaycomplaints/MainActivity.java
@@ -7,18 +7,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.example.barangaycomplaints.databinding.ActivityMainBinding;
+
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ItemAdapter itemAdapter;
+    private List<Complaint> complaintList;
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        itemAdapter = new ItemAdapter(complaintList);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setAdapter(itemAdapter);
     }
 }

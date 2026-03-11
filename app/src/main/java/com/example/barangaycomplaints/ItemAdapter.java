c055e5d0575af0ad83e8e6ac51626a7eb86bc21a
Sij-Coco <parkourweapon@gmail.com>
2026-03-10 21:18:43 -0800

    acivities, ItemAdapter and recycler view

---
+++ app/src/main/java/com/example/barangaycomplaints/ItemAdapter.java
@@ -0,0 +1,8 @@
+package com.example.barangaycomplaints;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+
+
+}

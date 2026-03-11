07d0fcfcbc5d27d93f6af3585bd6dfc7084b50df
Sij-Coco <parkourweapon@gmail.com>
2026-03-10 21:21:11 -0800

    ItemAdapter

--- app/src/main/java/com/example/barangaycomplaints/ItemAdapter.java
+++ app/src/main/java/com/example/barangaycomplaints/ItemAdapter.java
@@ -1,8 +1,33 @@
 package com.example.barangaycomplaints;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder> {
+    private List<Complaint> itemList;
 
 
+
+    @NonNull
+    @Override
+    public ItemAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemAdapter.ItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ItemViewHolder {
+    }
 }
